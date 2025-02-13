42ef8f8470324a05601766c7a488f72a093c3387
yrd01 <QueenRoksana05@gmail.com>
2025-02-13 01:55:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 document.getElementById("yes").addEventListener("click", () => {
+    document.querySelector(".background").style.opacity = "0";
+    setTimeout(() => {
+        document.querySelector(".background").style.background = "url('20240809_213832.jpg') no-repeat center center/cover";
+        document.querySelector(".background").style.opacity = "1";
+    }, 500);
+
     for (let i = 0; i < 20; i++) {
         createHeart();
     }
@@ -10,15 +16,15 @@ function createHeart() {
     heart.innerHTML = "❤️";
     document.body.appendChild(heart);
 
-    const size = Math.random() * 30 + 20; // Wielkość serc 20-50px
-    const startX = Math.random() * window.innerWidth; // Losowa pozycja startowa X
-    const startY = Math.random() * window.innerHeight; // Losowa pozycja startowa Y
+    const size = Math.random() * 30 + 20;
+    const startX = Math.random() * window.innerWidth;
+    const startY = Math.random() * window.innerHeight;
 
     heart.style.left = `${startX}px`;
     heart.style.top = `${startY}px`;
     heart.style.fontSize = `${size}px`;
 
-    const animationDuration = Math.random() * 2 + 3; // 3-5 sekund
+    const animationDuration = Math.random() * 2 + 3;
     heart.style.animation = `floatUp ${animationDuration}s ease-in-out forwards`;
 
     setTimeout(() => heart.remove(), animationDuration * 1000);
